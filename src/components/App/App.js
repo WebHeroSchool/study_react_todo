59e650ed53de4313c7ad1bad16ccbfda37db0bd7
fallemabc <fallemabc@gmail.com>
2020-01-21 23:53:36 +0600

    add setState in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,20 +17,36 @@ class App extends React.Component {
     items: [
       {
         value: 'Почитать книгу',
-        isDone: true
+        isDone: true,
+        id: 1
       },
       {
         value: 'Приготовить обед',
-        isDone: false
+        isDone: false, 
+        id: 2
       },
       {
         value: 'Помыть пол',
-        isDone: false
+        isDone: false, 
+        id: 3
       }
-    ]
+    ],
+    count: 3
   };
 
-  onClickDone = isDone => console.log(isDone);
+  onClickDone = id => {
+    const newItemList = this.state.items.map(item => {
+      const newItem = { ...item };
+
+      if (item.id === id) {
+          newItem.isDone = !item.isDone;
+      }
+
+      return newItem;
+    });
+
+    this.setState({ items: newItemList });
+  };
 
   render() {
     return (
@@ -49,7 +65,7 @@ class App extends React.Component {
           <ItemList items={this.state.items} onClickDone={this.onClickDone}/>
         </Grid>
         <Grid item xs>
-          <Footer count={3} />
+          <Footer count={this.state.count} />
         </Grid>
         </Grid>
 
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,7 +4,13 @@ import Item from '../Item/Item';
 
 const ItemList = ({ items, onClickDone }) => (<div>
     {items.map(item => <div key={item.value}>
-        <Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} /></div>)}
-    </div>);
+        <Item 
+            value={item.value} 
+            isDone={item.isDone} 
+            id={item.id}
+            onClickDone={onClickDone} 
+         />
+    </div>)}
+</div>);
                         
 export default ItemList;
